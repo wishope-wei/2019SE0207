c9824fa398667dd03d1ced4c901f535b98e5300d
wishope-wei <48470676+wishope-wei@users.noreply.github.com>
2019-03-12 15:56:18 +0800

    Update helloword.py

--- helloword.py
+++ helloword.py
@@ -1,7 +1,7 @@
 #!/usr/bin/evn python
 #-*- coding: utf-8 -*-
 '''
-Created on 2019年1月17日 Python 2.7
+Created on 2019年3月12日 Python 2.7
 @author: flatbean 
 实验要求：
 请先将本文件helloword.py上传到你新建的仓库
@@ -12,5 +12,5 @@ Created on 2019年1月17日 Python 2.7
 if __name__ == '__main__':
     print('Hello, I am first python script on Github')
     print('你好，我是在Github上的第一个python程序!\n')
-    print('你好，我的学号后四位是0101。')  
+    print('你好，我的学号后四位是0207。')  
     
